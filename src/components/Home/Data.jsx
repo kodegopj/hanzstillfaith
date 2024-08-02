e116ab5568707fa0fb13554ae384518132e76746
kodegopj <pauljohncunanan5@gmail.com>
2024-08-02 22:10:11 +0800

    fifth commit

--- src/components/Home/Data.jsx
+++ src/components/Home/Data.jsx
@@ -7,13 +7,13 @@ function Data() {
   return (
     <div className="home__data">
       <h1 className="home__title handy">
-        Hanz StillFaith  <img src={hand} alt="" className="hand" />{" "}
+        Hanz StillFaith  {" "}
       </h1>
       <h3 className="home__subtitle">Video Editor/Graphic Designer</h3>
       <p className="home__description">
-      I'm a Video Editor, Helping Businesses and Content Creators to ENGAGE their AUDIENCE with STUNNING VIDEOS not only Capture attention but turn Viewers into Loyal Customers.
+      I'm a Video Editor, <b className="unlock">Helping Businesses </b>and Content Creators to <b className="unlock">ENGAGE their AUDIENCE </b>with <b className="unlock">STUNNING VIDEOS </b>not only Capture attention but turn Viewers into Loyal Customers.
       </p>
-      <div className="home__description">"UNLOCK THE POWER OF VIDEO MARKETING: More Leads, More Sales, More Growth!"
+      <div className="home__description">"<b className="unlock">UNLOCK THE POWER OF VIDEO MARKETING:</b> More Leads, More Sales, More Growth!"
         </div>
       <a href="#contacts" className=" hello button--flex">
         Hello <RiMailSendFill className="icon__hello" />
